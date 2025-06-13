e61dfb9dcf588df01de1cc9ce111c38b348640fb
Virgiliu <plesca.virgiliu@gmail.com>
2025-06-13 09:53:41 +0300

    svdsf

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ import TheWelcome from './components/TheWelcome.vue'
 
     </div>
     <div>
-
+Hello there
     </div>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
